aeffbbc3df74912d7abcc24d7216ed26e61d0aa5
İlhan Cüvelek <75850897+ilhancuvelek@users.noreply.github.com>
2021-10-19 11:02:40 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("hello world")
+
+
+
+let dolarBugun=9.30
+
+let dolarDun=9.20
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun=11.2
+// euroDun=11
+
+console.log(euroDun)
+
+//array
+let konutKredileri=["konut","emlak","kamu komut"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+i+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
